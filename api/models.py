cda41221dbe5fdd4fc2c8f8c32bcaee791516f80
Prarabdh Garg <prarabdhgrg@gmail.com>
2021-08-24 18:31:51 +0530

    Add model for user session

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserSession(models.Model):
+    sessionPublicKey = models.CharField(max_length = 512)
+    patientId = models.IntegerField()
+    visitId = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+def __str__(self):
+    return self.sessionPublicKey
